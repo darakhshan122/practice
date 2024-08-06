17a2961f565a4142b3c7e27692b4c206a9aa84d7
darakhshan122 <physicswallahmodule872@gmail.com>
2024-08-06 10:40:58 +0530

    Add tailwind1st folder containing multiple files

---
+++ talwind1st/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+    
+
+ 
+    <h1 className="text-3xl font-bold underline  ring ring-pink-500 ring-offset-2 bg-slate-500 my-6  ">
+      Hello world!
+    </h1>
+    <button className='blur-none bg-red-950 text-cyan-100  my-10'>click</button>
+    <h3 className=' bg-slate-500 animate-bounce w-10 h-6'>down</h3>
+    <div className="grid my-11 ">
+    <p className='bg-gradient-to-r from-cyan-500 to-pink-400 place-self-center rounded-xl p-4'>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+
+    </div>
+
+    </>
+  )
+}
+
+export default App
